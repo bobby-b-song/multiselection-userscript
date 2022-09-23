1752a0bcdab8b9f7d1242e6e594e065a4f0947e5
Boyao Song <me@fla.si>
2022-09-23 22:45:34 +0100

    init commit

---
+++ userscript.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         multiselection
+// @namespace    http://fla.si/
+// @version      0.1
+// @description  notify you when it's a multiselection question, so you won't lost point when doing online quizzes
+// @author       Boyao SONG
+// @run-at       document-end
+// @match        http*://localhost:8000/*
+// @match        http*://*.ed.ac.uk/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.
+// @grant        none
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+	var input_checkboxes = document.querySelectorAll('form input[type="checkbox"]')
+	if (input_checkboxes.length >= 2) {
+        const element = document.createElement('p')
+        element.innerText = 'MULTI SELECTION ALERT'
+        element.style.color = 'red'
+        element.style.fontWeight = 'bold'
+        var parent = input_checkboxes[0].parentElement
+        parent.parentNode.insertBefore(element, parent)
+	}
+})();:%d
